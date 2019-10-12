681428d5d9a122bfd7c2203fc6d667b31816c5f0
Ryuk2punto0 <55495674+Ryuk2punto0@users.noreply.github.com>
2019-10-11 22:36:19 -0500

    otro

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{ public Transform spawnPosition;
+        public Rigidbody2D attack;
+        public float force = 5F;
+
+        public int hp;
+
+        protected void LaunchAttack()
+        {
+          Rigidbody2D attackClone =
+              Instantiate<Rigidbody2D>(
+                    attack, spawnPosition.position, spawnPosition.rotation);
+
+            attackClone.AddForce(transform.forward * force);
+        }
+
+        public void ApplyDamage(int damageValue)
+        {
+            hp -= damageValue;
+
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }}
