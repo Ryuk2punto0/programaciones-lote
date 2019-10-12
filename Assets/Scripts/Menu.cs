5e5e8968b7aee049c29e616222245e01a906f9a0
Susidu18 <susana_ml201@outlook.com>
2019-10-12 00:16:22 -0500

    Menu Inicio

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Inicio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inicio.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AbrirInicio()
+    {
+        Inicio.SetActive (true);
+    }
+
+    public void CambiareScena()
+    {
+        SceneManager.LoadScene("LOTE", LoadSceneMode.Single);
+    }
+}
