7a2702462056991944d94ac78d69717126ce85e6
Ryuk2punto0 <55495674+Ryuk2punto0@users.noreply.github.com>
2019-10-11 22:21:49 -0500

    lote

---
+++ Assets/Scripts/Attack1.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Attack1 : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    private Collider2D myCollider;
+    public ParticleSystem ps;
+
+    public int damage = 1;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        if (myRigidbody != null)
+        {
+            Invoke("AutoDestroy", 10F);
+
+            myCollider = GetComponent<Collider2D>();
+
+            if (myCollider != null)
+            {
+                myCollider.isTrigger = false;
+            }
+        }
+        else
+        {
+            AutoDestroy();
+        }
+    }
+
+    private void AutoDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player"
+            || col.gameObject.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<Character>().ApplyDamage(damage);
+        }
+
+        if (ps != null)
+        {
+            (Instantiate<ParticleSystem>(ps, transform.position, ps.transform.rotation)).Play();
+        }
+
+        AutoDestroy();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public int ni;
+	public GameObject p1;
+	public GameObject p2;
+	public GameObject p3;
+	//Textos
+	public GameObject t1;
+	public GameObject t2;
+	public GameObject t3;
+	public GameObject t4;
+	public GameObject t5;
+	public GameObject t6;
+    
+
+    void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	//void Update()
+	//{
+
+	//}
+
+ 
+    public void Invocar() {
+
+		ni = ni + 1;
+
+		if (ni == 1)
+		{ p1.SetActive(true); }
+		else if (ni == 2)
+		{ p2.SetActive(true); }
+		else if (ni == 3)
+		{ p3.SetActive(true); }
+	}
+
+	public void P1()
+	{
+		t1.SetActive(true);
+		t2.SetActive(false);
+		t3.SetActive(false);
+		t4.SetActive(false);
+		t5.SetActive(false);
+		t6.SetActive(false);
+
+	}
+
+	public void P2()
+	{
+		t1.SetActive(false);
+		t2.SetActive(true);
+		t3.SetActive(false);
+		t4.SetActive(false);
+		t5.SetActive(false);
+		t6.SetActive(false);
+
+	}
+
+	public void P3()
+	{
+		t1.SetActive(false);
+		t2.SetActive(false);
+		t3.SetActive(true);
+		t4.SetActive(false);
+		t5.SetActive(false);
+		t6.SetActive(false);
+
+	}
+
+	public void P4()
+	{
+		t1.SetActive(false);
+		t2.SetActive(false);
+		t3.SetActive(false);
+		t4.SetActive(true);
+		t5.SetActive(false);
+		t6.SetActive(false);
+
+	}
+
+	public void P5()
+	{
+		t1.SetActive(false);
+		t2.SetActive(false);
+		t3.SetActive(false);
+		t4.SetActive(false);
+		t5.SetActive(true);
+		t6.SetActive(false);
+
+	}
+
+	public void P6()
+	{
+		t1.SetActive(false);
+		t2.SetActive(false);
+		t3.SetActive(false);
+		t4.SetActive(false);
+		t5.SetActive(false);
+		t6.SetActive(true);
+
+	}
+}
