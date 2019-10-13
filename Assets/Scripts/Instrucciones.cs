c879b27d99ffacc7013176de6063b2b3e3d8d8b3
Susidu18 <susana_ml201@outlook.com>
2019-10-13 17:43:19 -0500

    instrucciones

--- Assets/Scripts/Instrucciones.cs
+++ Assets/Scripts/Instrucciones.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class Instrucciones : MonoBehaviour
 {
-    public GameObject Inicio;
+    public GameObject LetsBattle;
     // Start is called before the first frame update
     void Start()
     {
-        Inicio.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void AbrirInicio()
-    {
-        Inicio.SetActive (true);
-    }
+   
 
     public void CambiareScena()
     {
-        SceneManager.LoadScene("Instrucciones", LoadSceneMode.Single);
+        SceneManager.LoadScene("LOTE", LoadSceneMode.Single);
     }
 }
+
